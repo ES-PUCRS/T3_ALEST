3fa38a3cbe11337a328794edadf3befb614bdd6b
HCley <cleysonb.oliveira@gmail.com>
2019-11-23 15:30:41 -0300

    Linked

--- src/app.java
+++ src/app.java
@@ -1,5 +1,8 @@
 public class app {
     public static void main (String args[]){
-        LinkedList.test();
+    	LinkedList list = new LinkedList<Integer>();
+    	list.add (3);
+    	list.add (4);
+    	list.add (5);
     }
 }
