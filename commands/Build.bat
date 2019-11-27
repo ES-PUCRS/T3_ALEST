7462cc7be605a3ce206586319b5d5569ddd65bee
HCley <Cleysonb.Oliveira@gmail.com>
2019-11-27 16:52:22 -0200

    build pause just on error

--- commands/Build.bat
+++ commands/Build.bat
@@ -1,6 +1,7 @@
 SET mypath=%~dp0..\src
 cd %mypath%
 
+:run_again
 javac .\algorithms\datastructures\*.java .\algorithms\exceptions\*.java .\algorithms\tree\*.java .\*.java
 
-PAUSE
+if NOT ["%ERRORLEVEL%"]==["0"] PAUSE
