1ea1bc91f79c0b0715bd810820a4c506da298402
HCley <cleysonb.oliveira@gmail.com>
2019-12-02 01:39:46 -0300

    bash build doesnt need :run_clear anymore

--- commands/Build.bat
+++ commands/Build.bat
@@ -1,7 +1,6 @@
 SET mypath=%~dp0..\src
 cd %mypath%
 
-:run_again
 javac -d %mypath%\bin .\algorithms\datastructures\*.java .\exceptions\*.java .\algorithms\tree\*.java .\ui\*.java .\*.java
 
 if NOT ["%ERRORLEVEL%"]==["0"] PAUSE
